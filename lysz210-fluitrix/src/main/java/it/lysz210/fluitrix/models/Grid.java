c7ec1acd3de8b881e6884386daacdaba132b80c5
Lingyong Sun <lingyong.sun@gmail.com>
2025-07-18 10:49:09 +0200

    extract toElectrods

--- lysz210-fluitrix/src/main/java/it/lysz210/fluitrix/models/Grid.java
+++ lysz210-fluitrix/src/main/java/it/lysz210/fluitrix/models/Grid.java
@@ -7,13 +7,4 @@ public interface Grid {
     Coordinate2D move(Orientation direction);
     byte getCell(Coordinate2D position);
 
-    default boolean[][] toElectrods() {
-        var electrods = new boolean[this.getWidth()][this.getHeight()];
-        for (int x = 0; x < this.getWidth(); x++) {
-            for (int y = 0; y < this.getHeight(); y++) {
-                electrods[x][y] = this.getCell(new Coordinate2D(x, y)) > 0;
-            }
-        }
-        return electrods;
-    }
 }
---
+++ lysz210-fluitrix/src/main/java/it/lysz210/fluitrix/utils/GridToElectrodsMapper.java
@@ -0,0 +1,19 @@
+package it.lysz210.fluitrix.utils;
+
+import it.lysz210.fluitrix.models.Coordinate2D;
+import it.lysz210.fluitrix.models.Grid;
+
+import java.util.function.Function;
+
+public class GridToElectrodsMapper implements Function<Grid, boolean[][]> {
+    @Override
+    public boolean[][] apply(Grid grid) {
+        var electrods = new boolean[grid.getWidth()][grid.getHeight()];
+        for (int x = 0; x < grid.getWidth(); x++) {
+            for (int y = 0; y < grid.getHeight(); y++) {
+                electrods[x][y] = grid.getCell(new Coordinate2D(x, y)) > 0;
+            }
+        }
+        return electrods;
+    }
+}
